59f1ecb13796dd799b405f0a63bfc7087d3ddf47
André <a.silva14@hotmail.com>
2021-02-10 11:44:44 +0100

    Camera follow and put directions for the player

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+public class CameraScript : MonoBehaviour {
+
+    Rigidbody Rigidbody;
+    [SerializeField] float turnSpeed = 4.0f;
+    [SerializeField] Transform player;
+    private float angleBetween = 0.0f;
+    [SerializeField] Transform target;
+    private Vector3 offset;
+ 
+    void Start () {
+        Rigidbody = GetComponent<Rigidbody>();
+        offset = new Vector3(player.position.x, player.position.y + 3.0F , player.position.z + 7.0f);
+    }
+ 
+    void Update()
+    {
+        Vector3 targetDir = target.position - transform.position;
+        angleBetween = Vector3.Angle(transform.forward, targetDir);
+    }
+    
+    void LateUpdate()
+    {
+        offset = Quaternion.AngleAxis (Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
+        transform.position = player.position + offset; 
+        transform.LookAt(player.position);
+        
+    }
+    
+}
+
+
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,16 +4,36 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    //Variables
-   [SerializeField] private float speed_;
- 
-    void FixedUpdate() {
-        float h = Input.GetAxisRaw("Horizontal");
-        float v = Input.GetAxisRaw("Vertical");
-   
-        gameObject.transform.position = new Vector3 (transform.position.x + (h * speed_ * Time.fixedDeltaTime), 
-            transform.position.y + (0* speed_), transform.position.z + (v * speed_ * Time.fixedDeltaTime));
+    [SerializeField] Transform camPivot;
+    float heading = 0;
+    [SerializeField] Transform cam;
+    [SerializeField] float mouseSensitivity;
+    [SerializeField] private float speed;
+    Vector2 input;
+    void Update()
+
+    {
+
+        heading += Input.GetAxis("Mouse X") * Time.deltaTime * mouseSensitivity;
+
+        camPivot.rotation = Quaternion.Euler(0, heading, 0);
+        input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        input = Vector2.ClampMagnitude(input, 1);
+
+        Vector3 camF = cam.forward;
+        Vector3 camR = cam.right;
+
+        camF.y = 0;
+        camR.y = 0;
+        camF = camF.normalized;
+        camR = camR.normalized;
+
+        //transform.position += new Vector3(input.x,0,input.y) *Time.deltaTime”S5;
+
+        transform.position += (camF * input.y + camR * input.x) * Time.deltaTime * speed;
+
     }
+
 }
     
 
