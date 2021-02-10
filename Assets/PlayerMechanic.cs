5ad5fa6a88ef16bca31a096e59f2cd681f14a84f
LudoBernard <ludo.bernard.pro@gmail.com>
2021-02-10 11:47:08 +0100

    Can pickup mini cubes

---
+++ Assets/PlayerMechanic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMechanic : MonoBehaviour
+{
+    [SerializeField] private int miniCubesCount_;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.CompareTag("Minicube"))
+        {
+            miniCubesCount_++;
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnMiniCubes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+using Vector3 = UnityEngine.Vector3;
+
+public class SpawnMiniCubes : MonoBehaviour
+{
+    //Variables
+    [SerializeField] private GameObject miniCube_;
+    private float power_ = 7f;
+    private float radius_ = 5f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Instantiate(miniCube_, other.transform.position + Vector3.right, Quaternion.identity);
+                miniCube_.GetComponent<Rigidbody>().AddExplosionForce(power_, transform.position, radius_, 1f);
+            }
+            Destroy(gameObject);
+        }
+    }
+}
